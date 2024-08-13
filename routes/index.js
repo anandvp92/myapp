650c2c8c6546e68d9f8c4d385b26696f34b4a457
Anand V P <SE480@sitstech.local>
2024-08-13 15:30:27 +0530

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,59 @@
 var express = require('express');
 var router = express.Router();
-
+let products = [
+  {
+    name:"IPHONE SE",
+    category:"Mobile",
+    description:"This is a good phone",
+    image:"https://img.freepik.com/premium-photo/smartphone-balancing-with-pink-background_23-2150271746.jpg"
+  },
+  {
+    name:"IPHONE SE",
+    category:"Mobile",
+    description:"This is a good phone",
+    image:"https://img.freepik.com/premium-photo/smartphone-balancing-with-pink-background_23-2150271746.jpg"
+  },
+  {
+    name:"IPHONE SE",
+    category:"Mobile",
+    description:"This is a good phone",
+    image:"https://img.freepik.com/premium-photo/smartphone-balancing-with-pink-background_23-2150271746.jpg"
+  },
+  {
+    name:"IPHONE SE",
+    category:"Mobile",
+    description:"This is a good phone",
+    image:"https://img.freepik.com/premium-photo/smartphone-balancing-with-pink-background_23-2150271746.jpg"
+  },
+  {
+    name:"IPHONE SE",
+    category:"Mobile",
+    description:"This is a good phone",
+    image:"https://img.freepik.com/premium-photo/smartphone-balancing-with-pink-background_23-2150271746.jpg"
+  },
+  {
+    name:"IPHONE SE",
+    category:"Mobile",
+    description:"This is a good phone",
+    image:"https://img.freepik.com/premium-photo/smartphone-balancing-with-pink-background_23-2150271746.jpg"
+  },
+  {
+    name:"IPHONE SE",
+    category:"Mobile",
+    description:"This is a good phone",
+    image:"https://img.freepik.com/premium-photo/smartphone-balancing-with-pink-background_23-2150271746.jpg"
+  },
+  {
+    name:"IPHONE SE",
+    category:"Mobile",
+    description:"This is a good phone",
+    image:"https://img.freepik.com/premium-photo/smartphone-balancing-with-pink-background_23-2150271746.jpg"
+  }
+  
+]
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Index page' ,product:products});
 });
 
 module.exports = router;
